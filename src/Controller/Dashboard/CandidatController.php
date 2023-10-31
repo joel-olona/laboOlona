99b3b557065d98b212eb1c6098322f2c5d6641b7
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-10-31 14:14:27 +0300

    profile candidat step 1 & 2

--- src/Controller/Dashboard/CandidatController.php
+++ src/Controller/Dashboard/CandidatController.php
@@ -2,16 +2,37 @@
 
 namespace App\Controller\Dashboard;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\CandidateProfile;
+use App\Manager\ProfileManager;
+use App\Service\User\UserService;
+use App\Service\Mailer\MailerService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/dashboard/candidat')]
 class CandidatController extends AbstractController
 {
+    public function __construct(
+        private UserService $userService,
+        private EntityManagerInterface $em,
+        private MailerService $mailerService,
+        private ProfileManager $profileManager,
+        private RequestStack $requestStack,
+        private UrlGeneratorInterface $urlGenerator,
+    ){
+    }
+
     #[Route('/', name: 'app_dashboard_candidat')]
     public function index(): Response
     {
+        /** @var User $user */
+        $user = $this->userService->getCurrentUser();
+        if(!$user->getCandidateProfile() instanceof CandidateProfile)
+        return $this->redirectToRoute('app_profile');
         return $this->render('dashboard/candidat/index.html.twig', [
             'controller_name' => 'CandidatController',
         ]);
