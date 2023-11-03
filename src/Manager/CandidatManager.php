3236c29f22f7a55683d82e2036d90c9677e97922
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-11-03 18:29:21 +0300

    recommended postings

--- src/Manager/CandidatManager.php
+++ src/Manager/CandidatManager.php
@@ -3,9 +3,8 @@
 namespace App\Manager;
 
 use App\Entity\CandidateProfile;
-use DateTime;
-use App\Entity\Secteur;
-use Symfony\Component\Form\Form;
+use App\Entity\Entreprise\JobListing;
+use App\Repository\EntrepriseProfileRepository;
 use App\Service\User\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -17,11 +16,12 @@ class CandidatManager
         private EntityManagerInterface $em,
         private SluggerInterface $sluggerInterface,
         private RequestStack $requestStack,
+        private EntrepriseProfileRepository $entrepriseProfileRepository,
         private UserService $userService
     ){}
 
     
-    public function annoncesCandidat(CandidateProfile $candidat): array
+    public function annoncesCandidatDefaut(CandidateProfile $candidat): array
     {
         return array_slice(array_merge(
             $this->getPostingsByCandidatSectors($candidat),
@@ -33,61 +33,62 @@ class CandidatManager
     public function findExpertAnnouncements(CandidateProfile $candidat): array
     {
         return array_slice(array_merge(
-            $this->getPostingsByCandidatSectors($candidat),
             $this->getPostingsByCandidatSkills($candidat),
-            $this->getPostingsByCandidatLocalisation($candidat)
+            $this->getPostingsByCandidatLocalisation($candidat),
+            $this->getPostingsByCandidatSectors($candidat)
         ), 0, 6);
     }
     
     public function getPostingsByCandidatSectors(CandidateProfile $candidat): array
     {
-        $postings = [];
-        // $sectors = $candidat->getSecteurs();
-        // foreach ($sectors as $sector) {
-        //     $sectorEntreprises = $sector->getEntreprise();
-        //     foreach ($sectorEntreprises as $entreprise) {
-        //         $annonce = $entreprise->getJobListings();
-        //         if($posting->getStatus() === Posting::STATUS_PUBLISHED){
-        //             $postings[] = $posting;
-        //         }
-        //     }
-        // }
+        $annonces = [];
+        $sectors = $candidat->getSecteurs();
+        foreach ($sectors as $sector) {
+            $sectorEntreprises = $sector->getJobListings();
+            foreach ($sectorEntreprises as $posting) {
+                if($posting->getStatus() === JobListing::STATUS_PUBLISHED || $posting->getStatus() === JobListing::STATUS_FEATURED){
+                    $annonces[] = $posting;
+                }
+            }
+        }
 
-        return $postings;
+        return $annonces;
     }
     
     public function getPostingsByCandidatLocalisation(CandidateProfile $candidat): array
     {
-        $postings = [];
-        // $companies = $this->companyRepository->findBy([
-        //     'country' => $candidat->getCountry()
-        // ]);
-        // foreach ($companies as $company) {
-        //     $companyPostings = $company->getPostings();
-        //     foreach ($companyPostings as $posting) {
-        //         if($posting->getStatus() === Posting::STATUS_PUBLISHED){
-        //             $postings[] = $posting;
-        //         }
-        //     }
-        // }
+        $annonces = [];
 
-        return $postings;
+        $entreprise = $this->entrepriseProfileRepository->findBy([
+            'localisation' => $candidat->getLocalisation()
+        ]);
+
+        foreach ($entreprise as $company) {
+            $companyPostings = $company->getJobListings();
+            foreach ($companyPostings as $posting) {
+                if($posting->getStatus() === JobListing::STATUS_PUBLISHED || $posting->getStatus() === JobListing::STATUS_FEATURED){
+                    $annonces[] = $posting;
+                }
+            }
+        }
+
+        return $annonces;
     }
     
     public function getPostingsByCandidatSkills(CandidateProfile $candidat): array
     {
-        $postings = [];
-        // $skills = $candidat->getIdentity()->getTechnicalSkills();
-        // foreach ($skills as $skill) {
-        //     $skillPostings = $skill->getPostings();
-        //     foreach ($skillPostings as $posting) {
-        //         if($posting->getStatus() === Posting::STATUS_PUBLISHED){
-        //             $postings[] = $posting;
-        //         }
-        //     }
-        // }
+        $annonces = [];
+        $skills = $candidat->getCompetences();
+        foreach ($skills as $skill) {
+            $skillPostings = $skill->getJobListings();
+            foreach ($skillPostings as $posting) {
+                if($posting->getStatus() === JobListing::STATUS_PUBLISHED || $posting->getStatus() === JobListing::STATUS_FEATURED){
+                    $annonces[] = $posting;
+                }
+            }
+        }
 
-        return $postings;
+        return $annonces;
     }
 
 }
