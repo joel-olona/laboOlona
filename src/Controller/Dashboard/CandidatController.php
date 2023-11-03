f53e1f60a3e2bbc3cfc27495ccdfb3bc9c6fc99b
joel-olona <jrandriamalala.olona@gmail.com>
2023-11-03 16:27:43 +0100

    links dashboard entreprise

--- src/Controller/Dashboard/CandidatController.php
+++ src/Controller/Dashboard/CandidatController.php
@@ -3,6 +3,9 @@
 namespace App\Controller\Dashboard;
 
 use DateTime;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Repository\Entreprise\JobListingRepository;
 use App\Manager\ProfileManager;
 use App\Entity\CandidateProfile;
 use App\Entity\Entreprise\JobListing;
@@ -21,7 +24,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/dashboard/candidat')]
 class CandidatController extends AbstractController
-{
+ {
     public function __construct(
         private UserService $userService,
         private EntityManagerInterface $em,
@@ -43,36 +46,39 @@ class CandidatController extends AbstractController
 
     #[Route('/', name: 'app_dashboard_candidat')]
     public function index(Request $request): Response
-    {
-        $this->checkCandidat();
-        /** @var User $user */
-        $user = $this->userService->getCurrentUser();
-        $candidat = $user->getCandidateProfile();
-        $now = new DateTime();
-
-        $monday = clone $now;
-        $monday->modify('this monday');
-        $sunday = clone $monday;
-        $sunday->modify('+6 days');
-
-        $formatMonday = $monday->format('d');
-        $formatSunday = $sunday->format('d F Y');
-
-        $form = $this->createForm(AnnonceSearchType::class);
-        $form->handleRequest($request);
-
-        return $this->render('dashboard/candidat/index.html.twig', [
-            'identity' => $candidat,
-            // 'postings' => $this->postingManager->findExpertAnnouncements($expert),
-            'formatMonday' => $formatMonday,
-            'formatSunday' => $formatSunday,
-            'form' => $form->createView(),
-        ]);
-
-        return $this->render('dashboard/candidat/index.html.twig', [
-            'controller_name' => 'CandidatController',
-        ]);
+{
+    $this->checkCandidat();
+    /** @var User $user */
+    $user = $this->userService->getCurrentUser();
+    $candidat = $user->getCandidateProfile();
+    $query = $candidat->getSearchQuery();
+    if (!empty($query)) {
+        $offres = $this->searchPostings($query, $this->em);
     }
+    $now = new DateTime();
+
+    $monday = clone $now;
+    $monday->modify('this monday');
+    $sunday = clone $monday;
+    $sunday->modify('+6 days');
+
+    $formatMonday = $monday->format('d');
+    $formatSunday = $sunday->format('d F Y');
+
+    $form = $this->createForm(AnnonceSearchType::class);
+    $form->handleRequest($request);
+
+    
+
+    return $this->render('dashboard/candidat/index.html.twig', [
+        'identity' => $candidat,
+        // 'postings' => $this->postingManager->findExpertAnnouncements($expert),
+        'formatMonday' => $formatMonday,
+        'formatSunday' => $formatSunday,
+        'form' => $form->createView(),
+        // 'offres' => $offres,
+    ]);
+}
 
     private function searchPostings(string $query, EntityManagerInterface $entityManager): array
     {
@@ -105,6 +111,18 @@ class CandidatController extends AbstractController
             ->setParameters(array_merge($parameters, ['status' => JobListing::STATUS_PUBLISHED]));
     
         return $qb->getQuery()->getResult();
+    }
+
+     
+    #[Route("/profil", name: "profil")]
+     
+    public function profil(): Response
+    {
+        $candidat = $this->userService->getCurrentUser()->getCandidateProfile();
+
+        return $this->render('dashboard/candidat/profil.html.twig', [
+            // 'candidat' => $candidat,
+        ]);
     }
 
     #[Route('/annonces', name: 'app_dashboard_candidat_annonce')]
@@ -186,5 +204,11 @@ class CandidatController extends AbstractController
             'controller_name' => 'GuidesController',
         ]);
     }
+
+    // Voici les nouvelles structurations des routes
+
+   
+
+    
     
 }
