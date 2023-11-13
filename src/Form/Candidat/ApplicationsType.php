1d065a8c06622167b78572f1bad75fd4dfbfe527
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-11-13 18:13:06 +0300

    search candidature ajax moderateur

---
+++ src/Form/Candidat/ApplicationsType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form\Candidat;
+
+use App\Entity\Candidate\Applications;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ApplicationsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cvLink')
+            ->add('lettreMotivation', TextareaType::class, [
+                'label' =>  'Lettre de motivation',
+                'attr' => [
+                    'placeholder' => 'Bonjour ...',
+                    'rows' => 8
+                ]
+            ])
+            ->add('pretentionSalariale')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Applications::class,
+        ]);
+    }
+}
