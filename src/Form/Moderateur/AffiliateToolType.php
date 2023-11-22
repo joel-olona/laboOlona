72d66a09057db6d0263f19f72cae35cac07c018b
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-11-22 17:52:13 +0300

    integration ia with service woo

---
+++ src/Form/Moderateur/AffiliateToolType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form\Moderateur;
+
+use App\Entity\AffiliateTool;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AffiliateToolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('lienAffiliation')
+            ->add('commission')
+            ->add('slug')
+            ->add('type')
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AffiliateTool::class,
+        ]);
+    }
+}
