e5b9b54b7ce3d059510a29870ae8b41227902239
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2023-12-11 16:15:15 +0300

    debug redirection en boucle

--- src/Controller/Dashboard/CandidatController.php
+++ src/Controller/Dashboard/CandidatController.php
@@ -62,7 +62,7 @@ class CandidatController extends AbstractController
         $user = $this->userService->getCurrentUser();
         $candidat = $user->getCandidateProfile();
         if (!$candidat instanceof CandidateProfile){ 
-            return $this->redirectToRoute('app_connect');
+            return $this->redirectToRoute('app_profile');
         }
 
         return null;
