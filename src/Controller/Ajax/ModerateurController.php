1f6633c9525b3447ff4c69f13bf3576cc25ae54a
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2024-01-23 11:39:09 +0300

    modifier l'assignation dans profil

--- src/Controller/Ajax/ModerateurController.php
+++ src/Controller/Ajax/ModerateurController.php
@@ -6,12 +6,11 @@ use App\Entity\EntrepriseProfile;
 use App\Entity\Moderateur\Assignation;
 use App\Entity\Secteur;
 use App\Repository\Entreprise\JobListingRepository;
-use App\Repository\EntrepriseProfileRepository;
+use App\Repository\Moderateur\AssignationRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\HttpFoundation\Request;
 
 class ModerateurController extends AbstractController
@@ -19,6 +18,7 @@ class ModerateurController extends AbstractController
     public function __construct(
         private EntityManagerInterface $em,
         private JobListingRepository $jobListingRepository,
+        private AssignationRepository $assignationRepository,
     )
     {}
     
@@ -72,11 +72,72 @@ class ModerateurController extends AbstractController
             return [
                 'id' => $jobListing->getId(),
                 'titre' => $jobListing->getTitre(),
-                // Autres champs nécessaires
             ];
         }, $jobListings);
 
         return $this->json($annonces);
+    }   
+
+    #[Route('/ajax/edit/assignation', name: 'app_ajax_edit_assignation')]
+    public function editAssignation(Request $request): Response
+    {
+        $success = false;
+        $assignationId = $request->request->get('assignation_id');
+        $assignation = $this->assignationRepository->find($assignationId);
+
+        if ($assignation) {
+
+            $formHtml = $this->renderView('ajax/form/form_assignation.html.twig', [
+                'assignation' => $assignation,
+            ]);
+
+            $success = true;
+
+            return $this->json([
+                'success' => $success,
+                'form' => $formHtml,
+            ]);
+        } else {
+            // Gérer le cas où l'assignation n'est pas trouvée
+            return $this->json([
+                'assignation_id' => $assignationId,
+                'success' => $success,
+                'error' => 'Assignation non trouvée',
+            ]);
+        }
     }  
+
+    #[Route('/ajax/assignation/update/{id}', name: 'app_ajax_update_assignation')]
+    public function updateAssignation(Request $request, AssignationRepository $assignationRepository, $id): Response
+    {
+        $assignation = $assignationRepository->find($id);
+
+        if (!$assignation) {
+            return $this->json([
+                'success' => false,
+                'error' => 'Assignation non trouvée',
+            ]);
+        }
+        // Mettez à jour l'entité Assignation avec les nouvelles données
+        $assignation->setForfait($request->request->get('forfait'));
+        $assignation->setCommentaire($request->request->get('commentaire'));
+        
+        // Enregistrez les modifications dans la base de données
+        $this->em->persist($assignation);
+        $this->em->flush();
+        // $this->candidatManager->sendNotificationEmail($experience->getProfil());
+
+        return $this->json([
+            'success' => true,
+            'assignationId' => $assignation->getId(),
+            'forfait' => $assignation->getForfait(),
+            'rolePositionVisee' => $assignation->getRolePositionVisee(),
+            'commentaire' => $assignation->getCommentaire(),
+            'date' => $assignation->getDateAssignation()->format('d-m-Y'),
+            'entreprise' => $assignation->getJobListing()->getEntreprise()->getNom(),
+            'annonce' => $assignation->getJobListing()->getTitre(),
+            'error' => 'Assignation effectuée',
+        ]);
+    }
 
 }
