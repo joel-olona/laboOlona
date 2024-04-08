9964ce1560e98a2cc1b45521dcce7c03015f1700
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2024-04-08 20:24:29 +0300

    new serchform for entreprise

---
+++ src/Form/Search/Candidat/EntrepriseCandidateSearchType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form\Search\Candidat;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class EntrepriseCandidateSearchType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('query', TextType::class, [
+                'required' => false,
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Mots clés ...',
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'method' => 'GET',
+            'csrf_protection' => false,
+        ]);
+    }
+}
