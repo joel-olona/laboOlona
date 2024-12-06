a09be68f15e46063a2dbc132a04716efa3044982
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2024-12-06 22:23:13 +0300

    translate buttton

--- src/Form/Coworking/PlaceType.php
+++ src/Form/Coworking/PlaceType.php
@@ -8,13 +8,16 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PlaceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la place',
+            ])
             ->add('type', ChoiceType::class, [
                 'choices' => [
                     'Open space' => 'open_space',
@@ -23,13 +26,15 @@ class PlaceType extends AbstractType
             ])
             ->add('description')
             ->add('imageFile', VichImageType::class, [
+                'label' => 'Image',
                 'required' => false,
                 'allow_delete' => true, 
                 'download_uri' => true, 
                 'image_uri' => true,    
             ])
-            ->add('price')
-            ->add('isAvailable')
+            ->add('price', TextType::class, [
+                'label' => 'Prix',
+            ])
         ;
     }
 
