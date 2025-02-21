9f75e4898ca6286b68a0072027a6f3fb39288f18
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2025-02-21 18:17:53 +0300

    dashboard & profile template

--- assets/iary.js
+++ assets/iary.js
@@ -1,5 +1,9 @@
-
-$('.click-sub').on('click', '.sous-menu_', function(){
-		
-    $(this).siblings('.sub-nav-pills').toggle();
+$(function() {
+    document.addEventListener('turbo:load', handlePageLoad);
 });
+
+function handlePageLoad() {
+    $('.click-sub').on('click', '.sous-menu_', function(){            
+        $(this).siblings('.sub-nav-pills').toggle();
+    });
+}
