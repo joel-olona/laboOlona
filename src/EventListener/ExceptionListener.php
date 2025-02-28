aa2752aeefc2e81609ef68ad0c4027269291f72b
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2025-02-28 11:35:13 +0300

    debug redirect after auth

--- src/EventListener/ExceptionListener.php
+++ src/EventListener/ExceptionListener.php
@@ -3,48 +3,55 @@ namespace App\EventListener;
 
 use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ExceptionListener
 {
-    public function __construct(private Environment $twig)
+    public function __construct(
+        private Environment $twig,
+        private RequestStack $requestStack,
+        private RouterInterface $router,
+    )
     {}
 
     public function __invoke(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
-        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
+        $request = $this->requestStack->getCurrentRequest();
+        $session = $request->getSession();
+        $currentUri = $request->getUri(); // Récupérer l'URL demandée
 
+        if ($exception instanceof AccessDeniedException) {
+            $session->set('_security.main.target_path', $currentUri);
+
+            if (!$request->getUser()) { 
+                $response = new RedirectResponse($this->router->generate('app_login'));
+            } else {
+                $response = new RedirectResponse($currentUri);
+            }
+
+            $event->setResponse($response);
+            return; 
+        }
+
+        $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR;
         if ($exception instanceof HttpExceptionInterface) {
             $statusCode = $exception->getStatusCode();
-        }elseif ($exception instanceof AccessDeniedException) {
-            $statusCode = Response::HTTP_FORBIDDEN; 
         }
 
-        switch ($statusCode) {
-            case Response::HTTP_CONFLICT: // 409
-                $template = 'bundles/TwigBundle/Exception/error409.html.twig';
-                break;
-            case Response::HTTP_NOT_FOUND:
-                $template = 'bundles/TwigBundle/Exception/error404.html.twig';
-                break;
-            case Response::HTTP_FORBIDDEN:
-                $template = 'bundles/TwigBundle/Exception/error403.html.twig';
-                break;
-            case Response::HTTP_INTERNAL_SERVER_ERROR:
-                $template = 'bundles/TwigBundle/Exception/error500.html.twig';
-                break;
-            default:
-                $template = 'bundles/TwigBundle/Exception/error500.html.twig';
-                break;
-        }
+        $template = match ($statusCode) {
+            Response::HTTP_CONFLICT => 'bundles/TwigBundle/Exception/error409.html.twig',
+            Response::HTTP_NOT_FOUND => 'bundles/TwigBundle/Exception/error404.html.twig',
+            Response::HTTP_FORBIDDEN => 'bundles/TwigBundle/Exception/error403.html.twig',
+            default => 'bundles/TwigBundle/Exception/error500.html.twig',
+        };
 
-        $html = $this->twig->render($template, [
-            'message' => $exception->getMessage(),
-        ]);
-
+        $html = $this->twig->render($template, ['message' => $exception->getMessage()]);
         $response = new Response($html, $statusCode);
 
         if ($exception instanceof HttpExceptionInterface) {
