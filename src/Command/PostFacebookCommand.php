1a798ace5babe507daf02263879efd2b998c91fd
RochelManatsoa <nirinarochelmiandrisoa@gmail.com>
2025-04-18 15:26:25 +0300

    require city & mail job offer published

--- src/Command/PostFacebookCommand.php
+++ src/Command/PostFacebookCommand.php
@@ -2,13 +2,14 @@
 
 namespace App\Command;
 
-use App\Entity\Entreprise\JobListing;
 use App\Service\FacebookPublisher;
+use App\Entity\Entreprise\JobListing;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use App\Repository\EntrepriseProfileRepository;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
@@ -36,16 +37,17 @@ class PostFacebookCommand extends Command
     {
         $this
             ->setDescription('Publie une annonce sur la page Facebook Olona Talents')
+            ->addArgument('id', InputArgument::OPTIONAL, 'Id de l\'annonce à publier')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $annonces = $this->entityManager->getRepository(JobListing::class)->findJoblistingsForPostFacebook();
-        if(is_array($annonces) && count($annonces) > 0){
-            $annonce = $annonces[0];
-            $io->section('Publication annonces premium sur la page facebook Olona Talents...');
+        $id = $input->getArgument('id');
+        
+        if($id){
+            $annonce = $this->entityManager->getRepository(JobListing::class)->findOneById($id);
             $postText = sprintf(
                 "🚀 Saisissez votre chance : Devenez %s chez %s !\n\n🔥%s\n\n📍 Lieu : %s\n💼 Contrat : %s\n\nPostulez dès maintenant 👉 %s \n\n\n #Recrutement #Opportunité #Carrière",
                 $annonce->getTitre(),
@@ -65,8 +67,32 @@ class PostFacebookCommand extends Command
                 $output->writeln('<info>✅ Publication réussie !</info>');
                 return Command::SUCCESS;
             }
+        }else{
+            $annonces = $this->entityManager->getRepository(JobListing::class)->findJoblistingsForPostFacebook();
+            if(is_array($annonces) && count($annonces) > 0){
+                $annonce = $annonces[0];
+                $io->section('Publication annonces premium sur la page facebook Olona Talents...');
+                $postText = sprintf(
+                    "🚀 Saisissez votre chance : Devenez %s chez %s !\n\n🔥%s\n\n📍 Lieu : %s\n💼 Contrat : %s\n\nPostulez dès maintenant 👉 %s \n\n\n #Recrutement #Opportunité #Carrière",
+                    $annonce->getTitre(),
+                    $annonce->getEntreprise()->getNom(),
+                    $annonce->getShortDescription(),
+                    $annonce->getLieu(),
+                    $annonce->getTypeContrat(),
+                    $this->urlGenerator->generate('app_olona_talents_view_job_offer', ['id' => $annonce->getId()], UrlGeneratorInterface::ABSOLUTE_URL)
+                );
+        
+                $success = $this->facebookPublisher->publish($postText);
+        
+                if ($success) {
+                    $annonce->setIsPublishedOnFacebook(true);
+                    $this->entityManager->persist($annonce);
+                    $this->entityManager->flush();
+                    $output->writeln('<info>✅ Publication réussie !</info>');
+                    return Command::SUCCESS;
+                }
+            }
         }
-
 
         $output->writeln('<error>❌ La publication a échoué.</error>');
         
